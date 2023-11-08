d73a59d976bb2d4d627916ed3114be3d0cd9bade
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-08 15:21:59 +0100

    aggiunta badRequest exception

--- src/main/java/Esercizio7112023/Esercizio7112023/exceptions/ExceptionsHandler.java
+++ src/main/java/Esercizio7112023/Esercizio7112023/exceptions/ExceptionsHandler.java
@@ -9,9 +9,20 @@ import java.util.Date;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
+    @ExceptionHandler(BadRequest.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionPayload badRequest(BadRequest e){
+        return new ExceptionPayload(e.getMessage(),new Date());
+    }
 @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ExceptionPayload notFoundMethod(NotFoundException e){
     return new ExceptionPayload(e.getMessage(),new Date());
+}
+@ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionPayload internalError(Exception e){
+    e.printStackTrace();
+    return  new ExceptionPayload("Internal-server-error",new Date());
 }
 }
